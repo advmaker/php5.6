236fd3cdf7e89958d5de3cf03784bd4c6daed6f9
Vladimir N. Dyakov [DevOps TeamLead] <v.dyakov@alfaleads.net>
2021-10-05 13:50:34 +0300

    some changes

--- buildandpush.sh
+++ buildandpush.sh
@@ -1,5 +1,9 @@
-#git add -A .
-#git commit -m "some changes"
-#git push
-docker build -t advmaker/php5.6:latest .
+git add -A .
+git commit -m "some changes"
+git push
+
+docker build -f Dockerfile-5.6 -t advmaker/php5.6:latest .
 docker push advmaker/php5.6:latest
+
+docker build -f Dockerfile-5.6.opcache -t advmaker/php5.6-opcache:latest .
+docker push advmaker/php5.6-opcache:latest
